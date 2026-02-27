b592a30acdbbabed5cc7661239ce05e570174ec2
Soomin <soomin02094@gmail.com>
2026-02-27 23:26:38 +0900

    Lv 2-8 findByIdWithUser를 QueryDSL로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN FETCH t.user u " +
@@ -19,9 +17,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "AND (:endDate IS NULL OR t.modifiedAt <= :endDate) " +
             "ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllWithConditions(Pageable pageable, String weather, LocalDateTime startDate, LocalDateTime endDate);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
