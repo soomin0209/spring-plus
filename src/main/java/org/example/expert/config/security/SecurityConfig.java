5cfe5b1bd2b82cef3dcc4bd44c30901b4acef4cf
Soomin <soomin02094@gmail.com>
2026-03-03 16:10:58 +0900

    Lv 3-12-1 EC2 배포 환경 구성 및 health check API 구현

--- src/main/java/org/example/expert/config/security/SecurityConfig.java
+++ src/main/java/org/example/expert/config/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
